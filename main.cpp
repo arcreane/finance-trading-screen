7aac8f961c664576731e28f01eb334c051807f8d
zelmano <zelmano@DESKTOP-RVEJP68>
2025-11-25 16:32:52 +0100

    simplification du layout

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "MainWindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
     // Global stylesheet to visualize the layout structure
