cb707b9de78757d0f575a93634d5cf845d82f6d4
sylvainbauchet <sylvain.bauchet@efrei.net>
2026-01-08 14:42:47 +0100

    modofication du mainwindow.cpp pour bien ordonner les différentes parties

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,5 +1,7 @@
 #include "MainWindow.h"
 #include "TickerPlaceholder.h"
+#include "orderbook.h"
+#include "TradingBottomPanel.h"
 
 #include <QVBoxLayout>
 #include <QHBoxLayout>
@@ -21,25 +23,22 @@ void MainWindow::setupUi() {
     setCentralWidget(centralWidget);
     centralWidget->setStyleSheet("background-color: #121212; color: #ffffff;");
 
-    // 1. MAIN LAYOUT (RED BOX in sketch) - Horizontal Split
-    // Left Part (Yellow) vs Right Part (Zone 4)
+    // 1. MAIN LAYOUT
     QHBoxLayout* mainLayout = new QHBoxLayout(centralWidget);
     mainLayout->setContentsMargins(0, 0, 0, 0);
     mainLayout->setSpacing(1);
 
     // --- LEFT CONTAINER (YELLOW BOX) ---
-    // Vertical Split: Top (Pink) vs Bottom (Zone 5)
     QWidget* yellowContainer = new QWidget();
     QVBoxLayout* yellowLayout = new QVBoxLayout(yellowContainer);
     yellowLayout->setContentsMargins(0, 0, 0, 0);
     yellowLayout->setSpacing(1);
 
-    // 2. Secondary Toolbar / Ticker Placeholder
-    TickerPlaceholder *tickerWidget = new TickerPlaceholder(this);
-    mainLayout->addWidget(tickerWidget);
+    // SUPPRIMÉ ICI : Le TickerPlaceholder n'est plus ajouté au mainLayout
+    // TickerPlaceholder *tickerWidget = new TickerPlaceholder(this);
+    // mainLayout->addWidget(tickerWidget);
 
     // --- TOP LEFT CONTAINER (GREEN BOX) ---
-    // Vertical Split: Top (Zone 1) vs Bottom (Zone 2)
     QWidget* greenContainer = new QWidget();
     QVBoxLayout* greenLayout = new QVBoxLayout(greenContainer);
     greenLayout->setContentsMargins(0, 0, 0, 0);
@@ -47,51 +46,62 @@ void MainWindow::setupUi() {
 
     // ZONE 1: Pair Info (Top of Green)
     QFrame* zone1 = new QFrame();
-    zone1->setFixedHeight(50); // Fixed height for header
-    zone1->setStyleSheet("background-color: #1e1e1e; border: 1px solid #00ff00;"); // Green border
+    zone1->setFixedHeight(50);
+    zone1->setStyleSheet("background-color: #1e1e1e; border: 1px solid #00ff00;");
     QVBoxLayout* z1l = new QVBoxLayout(zone1);
     z1l->addWidget(new QLabel("ZONE 1: PAIR INFO"));
     greenLayout->addWidget(zone1);
 
+    // --- CHANGEMENT : AJOUT DU TICKER ICI (ENTRE ZONE 1 et ZONE 2) ---
+    TickerPlaceholder *tickerWidget = new TickerPlaceholder(this);
+    // On peut fixer une hauteur pour qu'il ressemble à une barre et ne prenne pas trop de place
+    // tickerWidget->setFixedHeight(60); // Décommentez si vous voulez une hauteur fixe
+    greenLayout->addWidget(tickerWidget);
+    // -----------------------------------------------------------------
+
     // ZONE 2: Chart (Bottom of Green)
     QFrame* zone2 = new QFrame();
-    zone2->setStyleSheet("background-color: #1e1e1e; border: 1px solid #00ff00;"); // Green border
+    zone2->setStyleSheet("background-color: #1e1e1e; border: 1px solid #00ff00;");
     QVBoxLayout* z2l = new QVBoxLayout(zone2);
     z2l->addWidget(new QLabel("ZONE 2: CHART"));
-    greenLayout->addWidget(zone2, 1); // Expands to fill Green
+    greenLayout->addWidget(zone2, 1); // Expands to fill remaining Green space
+
+    // Container PINK
+    QWidget* pinkContainer = new QWidget();
+    // Note: Si vous voulez que le Carnet d'ordre (Zone 3) soit à DROITE du graphique,
+    // changez QVBoxLayout en QHBoxLayout ici :
+    QHBoxLayout* pinkLayout = new QHBoxLayout(pinkContainer);
+    pinkLayout->setContentsMargins(0,0,0,0); // Bonnes pratiques pour éviter les marges doubles
+    pinkLayout->setSpacing(1);
 
     pinkLayout->addWidget(greenContainer, 3); // Green takes 75% of Pink width
 
-    // ZONE 3: Order Book (Right of Pink)
+    // ZONE 3: Order Book
     QFrame* zone3 = new QFrame();
-    zone3->setStyleSheet("background-color: #1e1e1e; border: 1px solid #ff00ff;"); // Pink/Magenta border
+    zone3->setStyleSheet("background-color: #1e1e1e; border: 1px solid #ff00ff;");
     QVBoxLayout* z3l = new QVBoxLayout(zone3);
-    // z3l->addWidget(new QLabel("ZONE 3: ORDER BOOK"));
     OrderBook* orderBook = new OrderBook(zone3);
     z3l->addWidget(orderBook);
     pinkLayout->addWidget(zone3, 1); // Zone 3 takes 25% of Pink width
 
     yellowLayout->addWidget(pinkContainer, 3); // Pink takes 75% of Yellow height
 
-    // ZONE 5: Open Orders (Bottom of Yellow)
+    // ZONE 5: Open Orders
     QFrame* zone5 = new QFrame();
-    zone5->setStyleSheet("background-color: #1e1e1e; border: 1px solid #ffff00;"); // Yellow border
+    zone5->setStyleSheet("background-color: #1e1e1e; border: 1px solid #ffff00;");
     QVBoxLayout* z5l = new QVBoxLayout(zone5);
-    // z5l->addWidget(new QLabel("ZONE 5: OPEN ORDERS / POSITIONS"));
     TradingBottomPanel* bottomPanel = new TradingBottomPanel(zone5);
     z5l->addWidget(bottomPanel);
-    yellowLayout->addWidget(zone5, 1); // Zone 5 takes 25% of Yellow height
+    yellowLayout->addWidget(zone5, 1);
 
-    mainLayout->addWidget(yellowContainer, 4); // Yellow takes 80% of width
+    mainLayout->addWidget(yellowContainer, 1); // Yellow takes remaining width (stretch 1)
 
     // --- RIGHT CONTAINER (ZONE 4) ---
-    // Order Entry
     QFrame* zone4 = new QFrame();
-    zone4->setFixedWidth(300); // Fixed width or weight
-    zone4->setStyleSheet("background-color: #1e1e1e; border: 1px solid #0000ff;"); // Blue border
+    zone4->setFixedWidth(300);
+    zone4->setStyleSheet("background-color: #1e1e1e; border: 1px solid #0000ff;");
     QVBoxLayout* z4l = new QVBoxLayout(zone4);
     z4l->addWidget(new QLabel("ZONE 4: ORDER ENTRY"));
 
-    mainLayout->addWidget(zone4, 0); // Fixed width, so stretch 0 or 1 doesn't matter much if fixed width is set, but let's rely on fixed width.
+    mainLayout->addWidget(zone4, 0);
 }
-
